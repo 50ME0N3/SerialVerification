9b4ed1d3831b611b4228d4c6822b4c3711a2f3e0
someone <antoine.dvt@eduge.ch>
2022-04-25 10:00:42 +0200

    added some file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 // include the Node.js 'path' module at the top of your file
 const path = require('path');
+//include serialport
+const SerialPort = require('serialport').SerialPort;
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -17,8 +19,13 @@ app.whenReady().then(() => {
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
+    //list all the available ports and put them in a json file
+    SerialPort.list().then(function(ports){
+        require('fs').writeFileSync('./ports.json', JSON.stringify(ports));
+    });
 });
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
+
--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-import { SerialPort } from "serialport";
-var getPortsList = (callback) => {
-    var portsList = [];
-
-    SerialPort.list((err, ports) => {
-        ports.forEach((port) => {
-            portsList.push(port.comName);
-        });
-
-        callback(null, portsList);
+$.getJSON("./ports.json", function(data) {
+    var items = [];
+    $.each(data, function(key, val) {
+        items.push("<li class='list-group-item' id='" + key + "'>" + val.path + "</li>");
     });
-};
 
-console.log(getPortsList);
+    $("<ul/>", {
+        "class": "serial-ports list-group list-group-flush",
+        html: items.join("")
+    }).appendTo("div.content");
+});
